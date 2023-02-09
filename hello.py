f160171882be87313667ee8d0f4595cfce6e8f4e
oscar del campo carpio <oscardelcampo2002@gmail.com>
2023-02-09 18:11:50 +0100

    Oscar

--- hello.py
+++ hello.py
@@ -4,8 +4,11 @@ def antonio():
 def samuel():
     print("Hola soy Samuel, compañero de DAW")
 
+def oscar():
+    print("Hola soy Óscar, pa las reggetonas ")
+
 if __name__ == "__main__":
     print("Say hello to class ")
     antonio()
     samuel()
-    
+    oscar()
