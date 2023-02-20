8bed0150ebeb55bfbabff4dd26f10de09bc2a7a5
Daniel Carrasco <dani10carrascovargas@gmail.com>
2023-02-20 18:21:01 +0100

    daniel

--- hello.py
+++ hello.py
@@ -4,8 +4,12 @@ def antonio():
 def samuel():
     print("Hola soy Samuel, compañero de DAW")
 
+def daniel():
+    print("Hola soy Daniel, compañero de DAW")
+
 if __name__ == "__main__":
     print("Say hello to class ")
     antonio()
     samuel()
+    daniel()
     
